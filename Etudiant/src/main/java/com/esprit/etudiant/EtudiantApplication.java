e1bcedd879050a3e346155f53255440162358d57
bechirgrali <grali.bechir@esprit.tn>
2025-03-06 00:12:31 +0100

    Crud+Cors+fixH2dataBase

--- Etudiant/src/main/java/com/esprit/etudiant/EtudiantApplication.java
+++ Etudiant/src/main/java/com/esprit/etudiant/EtudiantApplication.java
@@ -1,7 +1,11 @@
 package com.esprit.etudiant;
 
+import com.esprit.etudiant.Repository.EtudiantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EtudiantApplication {
@@ -9,5 +13,16 @@ public class EtudiantApplication {
     public static void main(String[] args) {
         SpringApplication.run(EtudiantApplication.class, args);
     }
+    @Autowired
+    private EtudiantRepository repository;
 
+    @Bean
+    ApplicationRunner init() {
+        return (args) -> {
+
+
+
+
+        };
+    }
 }
