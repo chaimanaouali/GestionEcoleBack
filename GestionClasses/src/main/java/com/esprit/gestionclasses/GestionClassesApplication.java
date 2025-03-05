64d125701baef80b256a4a3b553a44aa2f8a0b5a
moatez <moatez.ghoul@esprit.tn>
2025-03-05 21:56:55 +0100

    start

---
+++ GestionClasses/src/main/java/com/esprit/gestionclasses/GestionClassesApplication.java
@@ -0,0 +1,13 @@
+package com.esprit.gestionclasses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionClassesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionClassesApplication.class, args);
+    }
+
+}
